4eb6f4b43c5cfccc561748a08da522144d62551f
Anderson Cheng <andicheng@yahoo.com>
2016-12-20 10:21:09 -0800

    Initial Commit

---
+++ animation_demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  animation_demo
+//
+//  Created by ANDERSON CHENG on 12/20/16.
+//  Copyright © 2016 ANDERSON CHENG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
